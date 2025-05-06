edbad423425b9518e795a6e84853cefc5e28359a
djd <djd@davisca.org>
2025-05-06 11:12:49 -0700

    Slideshows

--- _bin/slideshow.rb
+++ _bin/slideshow.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'pathname'
+require 'fileutils'
 
 cwd = Pathname.new(File.expand_path(File.dirname(__FILE__)))
 root = cwd.join("..")
@@ -12,10 +13,18 @@ images = Dir.glob(root.join("images/events/**/*.{jpg}").to_s)
 
 images.each do |i|
   url = "/" + i.split("/")[7..-1].join("/")
-  caption = "Caption"
+
+  cf1 = "/" + i.split("/")[1..-2].join("/") + "/caption.txt"
+  caption = if File.exist?(cf1)
+    Pathname.new(cf1).read
+  else
+    FileUtils::Verbose.touch(cf1)
+    nil
+  end
+
   slides << '<div class="swiper-slide">'
-  slides << '  <img src="' + url + '" alt="' + caption + '" />'
-  slides << '  <div class="caption">' + caption + '</div>'
+  slides << '  <img src="' + url + '" alt="' + (caption || "Image") + '" />'
+  slides << '  <div class="caption">' + caption + '</div>' if caption
   slides << '</div>'
 end
 
